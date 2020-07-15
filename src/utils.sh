f398cd73b176ecf37cb7582fa6380f30b1fbb0f9
Your Name <you@example.com>
2020-07-15 20:37:48 +0000

    proxy cert

--- src/utils.sh
+++ src/utils.sh
@@ -58,6 +58,11 @@ function enable_proxy_if_needed () {
         echo "Set up the Proxy"
         adb shell "content update --uri content://telephony/carriers --bind proxy:s:"${p[0]}" --bind port:s:"${p[1]}" --where "mcc=310" --where "mnc=260""
 
+		echo "Install proxy certificate"
+		adb push ./5bc1dc75.0 /data/misc/user/0/cacerts-added/5bc1dc75.0
+		adb shell "su 0 chmod 644 /data/misc/user/0/cacerts-added/5bc1dc75.0"
+		adb reboot
+		
         adb unroot
       else
         echo "Please use http:// in the beginning!"
